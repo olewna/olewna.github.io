a819cf56dd70eba4934b22160c8e783c175ddb36
olewna <dajcie.suba2002@gmail.com>
2025-07-17 15:16:18 +0200

    projects animation

--- portfolio/src/app/main/components/projects/projects.component.ts
+++ portfolio/src/app/main/components/projects/projects.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ProjectsService } from '../../../shared/service/projects.service';
 import { Router } from '@angular/router';
 import { Project, ProjectModel } from '../../../shared/models/project.model';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-projects',
@@ -9,7 +10,11 @@ import { Project, ProjectModel } from '../../../shared/models/project.model';
   styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent implements OnInit {
-  constructor(private service: ProjectsService, private router: Router) {}
+  constructor(
+    private service: ProjectsService,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   protected projects: ProjectModel[] = [];
   protected loading: boolean = true;
@@ -19,17 +24,47 @@ export class ProjectsComponent implements OnInit {
     this.service.getProjects().subscribe({
       next: (res: Project[]) => {
         this.projects = res.map((x) => {
-          this.loading = false;
           return {
             ...x,
             languages: x.languages.map((lang) => lang.name).join(', '),
           };
         });
+
+        this.loading = false;
+        this.cdr.detectChanges();
+        this.animateCards();
       },
     });
   }
 
   goToDetails(id: string) {
     this.router.navigate(['details', id]);
+  }
+
+  private animateCards(): void {
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach((card) => {
+      const timeline = gsap.timeline();
+
+      timeline
+        .from(card.querySelector('.image'), {
+          opacity: 0,
+          y: -100,
+          duration: 0.7,
+        })
+        .from(card.querySelector('.language'), {
+          opacity: 0,
+          y: -100,
+          duration: 0.7,
+        })
+        .from(card.querySelector('.title'), {
+          opacity: 0,
+          y: -100,
+          duration: 0.7,
+        });
+
+      timeline.play(0);
+    });
   }
 }
