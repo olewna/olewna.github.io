ad7018e5a12746a3e8e8b739259b30cb34f7be43
olewna <dajcie.suba2002@gmail.com>
2025-07-17 15:44:22 +0200

    about me animation

--- portfolio/src/app/main/components/about/about.component.ts
+++ portfolio/src/app/main/components/about/about.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
-export class AboutComponent {}
+export class AboutComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    this.animateAbout();
+  }
+
+  private animateAbout(): void {
+    const about = document.querySelector('section');
+
+    if (!about) return;
+    const timeline = gsap.timeline();
+
+    timeline
+      .from(about.querySelector('.about-img'), {
+        opacity: 0,
+        y: -100,
+        duration: 0.7,
+        delay: 0.5,
+      })
+      .from(about.querySelector('.about'), {
+        opacity: 0,
+        y: -100,
+        duration: 0.7,
+      });
+
+    timeline.play(0);
+  }
+}
