b6dfb0d4295c6ea3c0ccb79c8703a8715717dab9
olewna <dajcie.suba2002@gmail.com>
2025-07-17 15:37:05 +0200

    project details animated

--- portfolio/src/app/main/components/details/details.component.ts
+++ portfolio/src/app/main/components/details/details.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ProjectsService } from '../../../shared/service/projects.service';
 import { Project } from '../../../shared/models/project.model';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-details',
@@ -13,7 +14,8 @@ export class DetailsComponent implements OnInit {
   constructor(
     private service: ProjectsService,
     private route: ActivatedRoute,
-    private location: Location
+    private location: Location,
+    private cdr: ChangeDetectorRef
   ) {}
 
   protected project: Project | null = null;
@@ -28,9 +30,64 @@ export class DetailsComponent implements OnInit {
 
         if (result.length > 0) {
           this.project = result[0];
+
+          this.cdr.detectChanges();
+
+          this.animateCard();
         }
       },
     });
+  }
+
+  private animateCard(): void {
+    const card = document.querySelector('.card');
+
+    if (!card) return;
+
+    const timeline = gsap.timeline();
+
+    timeline
+      .from(card.querySelector('.title'), {
+        opacity: 0,
+        y: -100,
+        duration: 0.5,
+      })
+      .from(card.querySelector('.technologies'), {
+        opacity: 0,
+        y: -100,
+        duration: 0.5,
+      })
+      .from(card.querySelectorAll('.language'), {
+        opacity: 0,
+        y: -100,
+        duration: 0.5,
+        stagger: 0.1,
+      })
+      .from(card.querySelectorAll('.image'), {
+        opacity: 0,
+        y: -100,
+        duration: 0.5,
+        stagger: 0.1,
+      })
+      .from(card.querySelectorAll('.video'), {
+        opacity: 0,
+        y: -100,
+        duration: 0.5,
+        stagger: 0.1,
+      })
+      .from(card.querySelector('.description'), {
+        opacity: 0,
+        y: -100,
+        duration: 0.5,
+      })
+      .from(card.querySelectorAll('.link'), {
+        opacity: 0,
+        y: -100,
+        duration: 0.5,
+        stagger: 0.1,
+      });
+
+    timeline.play(0);
   }
 
   goBack() {
