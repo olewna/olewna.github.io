34f4effdffe3def949899b64eab8ea8193d35344
olewna <dajcie.suba2002@gmail.com>
2025-07-17 14:55:29 +0200

    home animation

--- portfolio/src/app/main/components/home/home.component.ts
+++ portfolio/src/app/main/components/home/home.component.ts
@@ -1,8 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    gsap.to('.anim', {
+      opacity: 1,
+      y: 0,
+      duration: 0.1,
+      stagger: 0.5,
+    });
+  }
+}
